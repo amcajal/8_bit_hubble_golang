a44ad6251edd40ba386f6f6295179fdd7b747e3e
amcajal <amartin.glimpse23@gmail.com>
2022-03-12 02:13:26 +0100

    Go fmt the main file

--- 8bHubbleGolang.go
+++ 8bHubbleGolang.go
@@ -1,28 +1,26 @@
 package main
 
 import (
-	"8_bit_hubble_golang/param"
 	"8_bit_hubble_golang/galaxy"
-    "log"
-        "math/rand"
+	"8_bit_hubble_golang/param"
+	"log"
+	"math/rand"
 )
-
 
 func main() {
 
-    // Check parameters are correct
-    
-    if err := param.CheckParams(); err != nil {
-        log.Fatal(err)
-    }
-    
+	// Check parameters are correct
+	if err := param.CheckParams(); err != nil {
+		log.Fatal(err)
+	}
+
 	// Initialize seed
 	rand.Seed(param.Seed)
 
-        // Generate galaxy
-        if err := galaxy.GenerateGalaxy(); err != nil {
-                log.Fatal(err)
-        }
+	// Generate galaxy
+	if err := galaxy.GenerateGalaxy(); err != nil {
+		log.Fatal(err)
+	}
 
-        log.Println("Galaxy Generated!")
+	log.Println("Galaxy Generated!")
 }
