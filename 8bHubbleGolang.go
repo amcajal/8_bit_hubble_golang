f8c6b9769c6558cce4dc850867f56ea8c1a506ca
amcajal <amartin.glimpse23@gmail.com>
2022-03-12 02:12:21 +0100

    Update main prototype with new packages

--- 8bHubbleGolang.go
+++ 8bHubbleGolang.go
@@ -2,52 +2,27 @@ package main
 
 import (
 	"8_bit_hubble_golang/param"
-	"8_bit_hubble_golang/sprites"
-	"image"
-	"image/png"
-	"image/draw"
-	"math/rand"
-	"os"
+	"8_bit_hubble_golang/galaxy"
     "log"
+        "math/rand"
 )
 
 
 func main() {
 
     // Check parameters are correct
-    err := param.CheckParams()
-    if err != nil {
+    
+    if err := param.CheckParams(); err != nil {
         log.Fatal(err)
     }
     
 	// Initialize seed
 	rand.Seed(param.Seed)
-    
-    // Turn base64 string into png image
-    pngSprite := sprites.GetSprite(sprites.Small)
-    
-    // Create image of fixed dimensions
-    dim_x, dim_y := 500, 500
-    rgba := image.NewRGBA(image.Rect(0,0,dim_x,dim_y))
-    
-    // Print 20 sprites in "random" places
-    sr := pngSprite.Bounds() // Get the full image as a "source rectangle"
-    for i := 0; i < 20; i++ {
-        x_c := rand.Intn(dim_x);
-        y_c := rand.Intn(dim_y);
 
-        // Draw the pngSprite
-        dp := image.Pt(x_c, y_c) // Point in the destiny image where to start to paint the sprite
-        r := image.Rectangle{dp, dp.Add(sr.Size())}
-        draw.Draw(rgba, r, pngSprite, sr.Min, draw.Src)
-    }
-    
-    // Save image
-    writer, err := os.Create(param.OutputDir + "/" + param.PngName)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer writer.Close()
-    
-    png.Encode(writer, rgba)
+        // Generate galaxy
+        if err := galaxy.GenerateGalaxy(); err != nil {
+                log.Fatal(err)
+        }
+
+        log.Println("Galaxy Generated!")
 }
